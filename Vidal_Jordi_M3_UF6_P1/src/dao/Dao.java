ecef5965483bce46cd4839a84c0b984db6277f44
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2024-09-20 15:47:27 +0200

    Primer paso Modificar interface Dao

--- Vidal_Jordi_M3_UF6_P1/src/dao/Dao.java
+++ Vidal_Jordi_M3_UF6_P1/src/dao/Dao.java
@@ -1,8 +1,10 @@
 package dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Employee;
+import model.Product;
 
 public interface Dao {
 	public void connect() throws SQLException;
@@ -10,4 +12,9 @@ public interface Dao {
 	public Employee getEmployee(int employeeId, String password);
 
 	public void disconnect() throws SQLException;
+	
+	public ArrayList<Product> getInventory();
+	
+	public boolean writeInventory(ArrayList<Product> inventory);
+
 }
--- Vidal_Jordi_M3_UF6_P1/src/dao/DaoImplJDBC.java
+++ Vidal_Jordi_M3_UF6_P1/src/dao/DaoImplJDBC.java
@@ -1,12 +1,14 @@
-package dao;
+	package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Employee;
+import model.Product;
 
 public class DaoImplJDBC implements Dao{
 
@@ -48,6 +50,18 @@ public class DaoImplJDBC implements Dao{
 		if (connection != null) {
 			connection.close();
 		}
+	}
+
+	@Override
+	public ArrayList<Product> getInventory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean writeInventory(ArrayList<Product> inventory) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
