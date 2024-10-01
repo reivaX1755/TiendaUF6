f430499061e9ff66aeee31dc05dd6a89d05deda0
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2024-10-01 20:55:07 +0200

    1. Refactor carga inicial inventario en base a un fichero
    2. Exportar datos inventario a fichero
    final

--- Vidal_Jordi_M3_UF6_P1/src/main/Logable.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public interface Logable {
-	boolean login(int user, String password);
-}
--- Vidal_Jordi_M3_UF6_P1/src/main/Payable.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-import model.Amount;
-
-public interface Payable {
-	boolean pay(Amount amount);
-}
